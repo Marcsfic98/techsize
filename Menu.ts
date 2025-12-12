6561fe2ea3b987d1dc53aa7db5ba6ec8bf55cabc
Marcos Ribeiro <131191211+Marcsfic98@users.noreply.github.com>
2025-12-12 09:23:21 -0300

    MENU CRIADO

--- Menu.ts
+++ Menu.ts
@@ -1,10 +1,13 @@
 import readlinesync = require("readline-sync");
 import { colors } from "./utils/Colors";
 
+let opcao :number
 
 
+    while(true){
 
-        console.log(colors.bg.black, colors.fg.magenta, 
+   
+        console.log(colors.bg.black, colors.fg.magentastrong, 
                     "------------------------------------------------------");
         console.log("                                                     ");
         console.log("                      TECHSIZE                       ");
@@ -19,3 +22,51 @@ import { colors } from "./utils/Colors";
         console.log("            6 - SAIR                                  ");
         console.log("------------------------------------------------------");
         console.log("                                                     ", colors.reset);
+
+
+         console.log("SELECIONE A OPCAO DESEJADA: ");
+        opcao = readlinesync.questionInt("");
+
+
+
+          if (opcao == 6) {
+            console.log(colors.fg.greenstrong, "\nOBRIGADO POR UTILIZAR NOSSO SISTEMA!");
+            console.log(colors.reset, "");
+            process.exit(0);
+        }
+
+        switch(opcao){
+            case 1:
+                console.log(colors.fg.whitestrong, "\n\nADICIONAR PRODUTO\n\n", colors.reset)
+                keyPress()
+                break
+
+            case 2:
+                console.log(colors.fg.whitestrong, "\n\nREMOVER PRODUTO\n\n", colors.reset)
+                keyPress()  
+                break
+                
+            case 3:
+                console.log(colors.fg.whitestrong, "\n\nEDITAR PRODUTO\n\n", colors.reset) 
+                keyPress()
+                break
+                
+            case 4:
+                console.log(colors.fg.whitestrong, "\n\nLISTAR PRODUTOS\n\n", colors.reset) 
+                keyPress()   
+                break
+
+            case 5:
+                console.log(colors.fg.whitestrong, "\n\nMOSTRAR PRODUTO\n\n", colors.reset)  
+                keyPress()  
+                break    
+        }
+
+         }
+
+
+ function keyPress(): void {
+    console.log(colors.reset, "");
+    console.log("\nPressione enter para continuar...");
+    readlinesync.prompt();
+}
