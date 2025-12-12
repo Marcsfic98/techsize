3ffe4e2ea407e75b2a7b28281081f59ac6521e3b
Marcos Ribeiro <131191211+Marcsfic98@users.noreply.github.com>
2025-12-12 09:10:34 -0300

    CRIADO PROJETO DE CONTROLE DE ESTOQUE TECHSIZE

---
+++ Menu.ts
@@ -0,0 +1,21 @@
+import readlinesync = require("readline-sync");
+import { colors } from "./utils/Colors";
+
+
+
+
+        console.log(colors.bg.black, colors.fg.magenta, 
+                    "------------------------------------------------------");
+        console.log("                                                     ");
+        console.log("                      TECHSIZE                       ");
+        console.log("                                                     ");
+        console.log("------------------------------------------------------");
+        console.log("                                                     ");
+        console.log("            1 - ADICIONAR PRODUTO                     ");
+        console.log("            2 - REMOVER PRODUTO                       ");
+        console.log("            3 - ALTERAR PRODUTO                       ");
+        console.log("            4 - LISTAR TODOS PRODUTOS                 ");
+        console.log("            5 - MOSTRAR  PRODUTO SELECIONADO          ");
+        console.log("            6 - SAIR                                  ");
+        console.log("------------------------------------------------------");
+        console.log("                                                     ", colors.reset);
